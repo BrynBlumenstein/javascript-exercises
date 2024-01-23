8f241c5156bdbe67be3f96fed16203a1db93b35a
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2024-01-22 20:34:55 -0500

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  tempC = (tempF - 32) * (5/9);
+  roundedTempC = Math.round(tempC * 10) / 10;
+  return roundedTempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  tempF = (tempC * (9/5)) + 32;
+  roundedTempF = Math.round(tempF * 10) / 10;
+  return roundedTempF;
 };
 
 // Do not edit below this line
