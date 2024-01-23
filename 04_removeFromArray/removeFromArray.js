718e9f5fd071de980ab5867d0bc22d81087ad681
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2024-01-22 19:49:49 -0500

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    return array.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
