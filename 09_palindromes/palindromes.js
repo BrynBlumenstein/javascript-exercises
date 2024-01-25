160d177a2490f217eba6966c57720aeaf8c8bc51
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2024-01-25 18:42:22 -0500

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // remove non-alphanumeric characters from string and convert all letters to lower case
+    string = (string.replace(/[^a-zA-Z0-9]/g, '')).toLowerCase();
 
+    // reverse the string
+    const characters = string.split('');
+    const reversedArray = characters.reverse();
+    const reversedString = reversedArray.join('');
+
+    // determine if string is a palindrome
+    if (string === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
