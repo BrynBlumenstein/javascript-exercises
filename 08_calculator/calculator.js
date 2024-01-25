8e6724291c80da8ea05a6025cb55a5167231e40f
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2024-01-25 18:16:28 -0500

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(operand1, operand2) {
+	return operand1 + operand2;
 };
 
-const subtract = function() {
-	
+const subtract = function(operand1, operand2) {
+	return operand1 - operand2;
 };
 
-const sum = function() {
-	
+const sum = function(operands) {
+  let sum = 0;
+	for (let operand of operands) sum += operand;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(operands) {
+  let product = 1;
+	for (let operand of operands) product *= operand;
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let product = 1;
+	for (let i = 0; i < exponent; i++) product *= base;
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  product = 1;
+	for (let i = number; i > 0; i--) product *= i;
+  return product;
 };
 
 // Do not edit below this line
