518c10d9d2291d78f4f2a4df305d35b01fe6c067
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2024-01-25 19:13:52 -0500

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const CURRENT_YEAR = 2024;
+    let maxAge = 0;
+    let oldest;
 
+    people.forEach(person => {
+        if (person.hasOwnProperty('yearOfDeath')) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = CURRENT_YEAR - person.yearOfBirth;
+        }
+        
+        if (person.age > maxAge) {
+            maxAge = person.age;
+            oldest = person;
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
